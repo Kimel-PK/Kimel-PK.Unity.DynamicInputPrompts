e21861f7a34bdf89b94546cb480cecdbb6388afb
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2024-12-21 14:19:17 +0100

    Exclude Switch support from Linux build

--- Runtime/Scripts/Device Detectors/SwitchGamepadDetector.cs
+++ Runtime/Scripts/Device Detectors/SwitchGamepadDetector.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+#if !UNITY_STANDALONE_LINUX
 using UnityEngine.InputSystem.Switch;
+#endif
 
 namespace KimelPK.DynamicInputPrompts {
 	
 	[CreateAssetMenu (fileName = "SwitchGamepadDetector", menuName = "Kimel-PK/DynamicInputPrompts/DeviceDetectors/SwitchGamepadDetector")]
 	public class SwitchGamepadDetector : DeviceDetector {
-
+		
+#if !UNITY_STANDALONE_LINUX
 		public override bool DetectDevice (InputDevice inputDevice) => inputDevice is SwitchProControllerHID;
+#else
+		public override bool DetectDevice (InputDevice inputDevice) => false;
+#endif
 	}
 }
